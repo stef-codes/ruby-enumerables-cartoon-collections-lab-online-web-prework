25b39ff4f9072105616241e61950015431bd8aaa
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-27 04:51:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
