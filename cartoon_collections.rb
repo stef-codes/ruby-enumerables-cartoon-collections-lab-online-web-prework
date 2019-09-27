46966bbb20a7789590c0829af88a52cbeb659c4d
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-27 05:34:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,14 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  i = 0 
+  if  array.include?(cheese_types) {|i| i.length}
+    return true
+  else 
+    return false
+  i += 1
+  end 
+end
 end
