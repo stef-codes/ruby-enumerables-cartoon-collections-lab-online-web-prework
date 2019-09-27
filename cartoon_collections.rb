801c0e1ae05585b4749eff6da7aea25ed8ca5c7b
Stefanie Davis <davisstefanie2010@gmail.com>
2019-09-27 04:49:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |item, index| puts "#{index+1}. #{item}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|call| call.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
